1c90950acc45501d87b54fabc51ed29f6c40ac86
Jeremy Miller <jeremy.miller@clarivate>
2021-02-19 11:24:09 -0800

    add springboot to project

---
+++ console/src/main/java/com/jeremy/Main.java
@@ -0,0 +1,19 @@
+package com.jeremy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    log.info("Guess the number game");
+
+    SpringApplication.run(Main.class, args);
+
+  }
+}
